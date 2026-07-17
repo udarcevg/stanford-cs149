a3da10ff89025ddbf999217759008cfab66a7a35
Udarcev George <udarcev@gmail.com>
2026-07-17 16:37:02 +0500

    Program 5 success

--- asst1/prog5_saxpy/main.cpp
+++ asst1/prog5_saxpy/main.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <algorithm>
-
+#include <arm_neon.h>
 #include "CycleTimer.h"
 #include "saxpy_ispc.h"
 
@@ -29,6 +29,29 @@ static void verifyResult(int N, float* result, float* gold) {
 using namespace ispc;
 
 
+
+void saxpyNeon(int N, float scale, float* X, float* Y, float* result) {
+    float32x4_t vscale = vdupq_n_f32(scale);
+
+    int i = 0;
+
+    for (; i <= N - 4; i += 4) {
+        float32x4_t vx = vld1q_f32(&X[i]);
+        float32x4_t vy = vld1q_f32(&Y[i]);
+
+        // result = scale * X + Y
+        float32x4_t vres = vmlaq_f32(vy, vx, vscale);
+
+        vst1q_f32(&result[i], vres);
+    }
+
+    // cleanup for remaining elements
+    for (; i < N; i++) {
+        result[i] = scale * X[i] + Y[i];
+    }
+}
+
+
 int main() {
 
     const unsigned int N = 20 * 1000 * 1000; // 20 M element vectors (~80 MB)
@@ -42,6 +65,7 @@ int main() {
     float* resultSerial = new float[N];
     float* resultISPC = new float[N];
     float* resultTasks = new float[N];
+    float* resultNeon = new float[N];
 
     // initialize array values
     for (unsigned int i=0; i<N; i++)
@@ -51,6 +75,7 @@ int main() {
         resultSerial[i] = 0.f;
         resultISPC[i] = 0.f;
         resultTasks[i] = 0.f;
+        resultNeon[i] = 0.f;
     }
 
     //
@@ -65,12 +90,12 @@ int main() {
         minSerial = std::min(minSerial, endTime - startTime);
     }
 
-// printf("[saxpy serial]:\t\t[%.3f] ms\t[%.3f] GB/s\t[%.3f] GFLOPS\n",
-    //       minSerial * 1000,
-    //       toBW(TOTAL_BYTES, minSerial),
-    //       toGFLOPS(TOTAL_FLOPS, minSerial));
+    printf("[saxpy serial]:\t\t[%.3f] ms\t[%.3f] GB/s\t[%.3f] GFLOPS\n",
+              minSerial * 1000,
+              toBW(TOTAL_BYTES, minSerial),
+              toGFLOPS(TOTAL_FLOPS, minSerial));
 
-    //
+
     // Run the ISPC (single core) implementation
     //
     double minISPC = 1e30;
@@ -99,14 +124,27 @@ int main() {
         minTaskISPC = std::min(minTaskISPC, endTime - startTime);
     }
 
-    verifyResult(N, resultTasks, resultSerial);
-
     printf("[saxpy task ispc]:\t[%.3f] ms\t[%.3f] GB/s\t[%.3f] GFLOPS\n",
            minTaskISPC * 1000,
            toBW(TOTAL_BYTES, minTaskISPC),
            toGFLOPS(TOTAL_FLOPS, minTaskISPC));
 
+
+    double minNeon = 1e30;
+    for (int i = 0; i < 3; ++i) {
+        double startTime = CycleTimer::currentSeconds();
+        saxpyNeon(N, scale, arrayX, arrayY, resultNeon);
+        double endTime = CycleTimer::currentSeconds();
+        minNeon = std::min(minNeon, endTime - startTime);
+    }
+    verifyResult(N, resultNeon, resultSerial);
+    printf("[saxpy neon]:\t\t[%.3f] ms\t[%.3f] GB/s\t[%.3f] GFLOPS\n",
+       minNeon * 1000,
+       toBW(TOTAL_BYTES, minNeon),
+       toGFLOPS(TOTAL_FLOPS, minNeon));
+
     printf("\t\t\t\t(%.2fx speedup from use of tasks)\n", minISPC/minTaskISPC);
+    printf("\t\t\t\t(%.2fx speedup from NEON)\n", minSerial / minNeon);
     //printf("\t\t\t\t(%.2fx speedup from ISPC)\n", minSerial/minISPC);
     //printf("\t\t\t\t(%.2fx speedup from task ISPC)\n", minSerial/minTaskISPC);
 
@@ -115,6 +153,7 @@ int main() {
     delete[] resultSerial;
     delete[] resultISPC;
     delete[] resultTasks;
+    delete[] resultNeon;
 
     return 0;
 }
